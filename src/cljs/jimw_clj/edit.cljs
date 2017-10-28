820446cece515b0dbcc0c942a2e957b509694105
chanshunli <chanshunli@gmail.com>
2017-10-28 23:52:02 +0800

    单个文件(单篇文章)=>独立出去文件(独立出去完整的文章)

---
+++ src/cljs/jimw_clj/edit.cljs
@@ -0,0 +1,35 @@
+(ns jimw-clj.edit
+  (:require [reagent.core :as r]))
+
+(defn blog-name-input-par [{:keys [id name on-save on-stop]}]
+  (let [val (r/atom name)
+        stop #(do (reset! val "")
+                  (if on-stop (on-stop)))
+        save #(let [v (-> @val str clojure.string/trim)]
+                (if-not (empty? v) (on-save v))
+                (stop))]
+    (fn [{:keys [id class placeholder]}]
+      [:input.input-par {:type "text" :value @val
+                         :id id :class class :placeholder placeholder
+                         :on-blur save
+                         :on-change #(reset! val (-> % .-target .-value))
+                         :on-key-down #(case (.-which %)
+                                         13 (save)
+                                         27 (stop)
+                                         nil)}])))
+
+(def blog-name-edit (with-meta blog-name-input-par
+                      {:component-did-mount #(.focus (r/dom-node %))}))
+
+(defn blog-name-item []
+  (let [editing (r/atom false)]
+    (fn [{:keys [id name save-fn]}]
+      [:li {:class (str (if @editing "editing"))}
+       [:div.view
+        [:h1
+         [:label {:on-double-click #(reset! editing true)} name]]]
+       (when @editing
+         [blog-name-edit {:class "edit"
+                          :on-save  #(save-fn id %)
+                          :name name
+                          :on-stop #(reset! editing false)}])])))
