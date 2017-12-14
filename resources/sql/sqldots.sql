4fb39fcb096190033a5f01d98a2cf383c08de178
Steve Chan <chanshunli@gmail.com>
2017-12-14 15:27:39 +0800

    Add sqldots dataframe 二维表 保存 sql dot 节点 和 关系链

---
+++ resources/sql/sqldots.sql
@@ -0,0 +1,30 @@
+CREATE TYPE DOT_TYPE AS ENUM (
+  'TABLE',
+  'RELATION'
+);
+
+CREATE CAST ( VARCHAR AS DOT_TYPE )
+WITH INOUT AS IMPLICIT;
+
+CREATE TABLE sqldots (
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  content TEXT NOT NULL,
+  dot_type DOT_TYPE,
+  defmodel INT,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE SEQUENCE sqldots_new_id_seq
+    START WITH 4859
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE sqldots_new_id_seq OWNER TO jim;
+
+ALTER SEQUENCE sqldots_new_id_seq OWNED BY sqldots.id;
+
+ALTER TABLE ONLY sqldots ALTER COLUMN id SET DEFAULT nextval('sqldots_new_id_seq'::regclass);
