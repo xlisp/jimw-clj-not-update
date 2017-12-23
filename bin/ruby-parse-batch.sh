5b9bc9aa784e2bc2ab94c48499340561a476f37d
Steve Chan <chanshunli@gmail.com>
2017-12-24 06:21:01 +0800

    莫名错误

--- bin/ruby-parse-batch.sh
+++ bin/ruby-parse-batch.sh
@@ -14,3 +14,7 @@ echo "Ruby file count: " ` find $rbpath -name '*.rb' | wc -l `
 ## 只有几个测试文件才有这样的错误解析, TODOS: 变成AST之前,就要过滤掉 "\xBE"
 ag "error: literal contains escape sequences incompatible with UTF-8" | awk -F":" '{print $1}' | xargs -I file rm file
 
+## Don't know how to create ISeq from: clojure.lang.Symbol => 不知是什么错误, .e.g: lib/ruby-jimw-code/rails/actionpack/test/dispatch/request/session_test.rb.ast
+## TODOS: 先删除所有的测试先, 后面恢复,不要卡住大脑流
+find $rbpath -name  "*_test.rb.ast" | xargs -I file rm file
+
