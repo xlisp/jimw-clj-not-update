7975335637c77dac38eaa394c61acf558e6cd25b
Steve Chan <chanshunli@gmail.com>
2017-12-23 11:57:18 +0800

    jim2: 监听多次的DB变化, 可以终止的循环

--- src/clj/jimw_clj/db/streaming.clj
+++ src/clj/jimw_clj/db/streaming.clj
@@ -35,8 +35,28 @@
 ;; jim0: 只能监听一次的DB变化
 ;; (prn (<!! stream-source))
 
-;; jim1: 监听多次的DB变化
+;; jim1: 监听多次的DB变化 => 不可终止的循环,只能留在里面,即内存泄露了
 #_(a/go-loop []
     (let [x (<!! stream-source)]
       (println "Got a value in this loop:" x))
     (recur))
+
+;; jim2: 监听多次的DB变化, 可以终止的循环
+#_(def aaa
+  (let [stop-ch (a/promise-chan)]
+    (a/go-loop []
+      (let [[v port] (a/alts! [stop-ch stream-source] :priority true)]
+        (when-not (= port stop-ch)
+          (println "Message: " v "\nFrom Object: " port)
+          )
+        )
+      #_(let [x (<!! stream-source)]
+          (println "Got a value in this loop:" x))
+      (recur))
+    stop-ch))
+;; =>
+;; Message:  {:change [{:kind update, :schema public, :table blogs, ... }
+;; From Object:  #object[clojure.core.async.impl.channels.ManyToManyChannel...]
+
+;; 终止go-loop循环
+;; (a/close! aaa) ;; => nil
