9b0707932a73d5cf11c5c7c8994e0d8a6e4859b7
Steve Chan <chanshunli@gmail.com>
2017-12-28 09:51:12 +0800

    Next: 把postwalk做成一个结构搜索: 搜索define结构, **复合算法结构

--- src/clj/jimw_clj/db/scheme.clj
+++ src/clj/jimw_clj/db/scheme.clj
@@ -5,7 +5,6 @@
    [clojure.pprint :as pp]
    [taoensso.timbre :refer [error debug info]]))
 
-;; 测试新的项目导入是否解析报错:
 ;; (read-string-for-pro (fn [code-list file-name] (map first code-list)) "ydiff")
 (defn read-string-for-pro
   [op-fn & project]
@@ -36,14 +35,10 @@
                       (str/replace "\\'" "back-slant31-que")
                       (str/replace "^" "-back-slant32-que")
                       (str/replace "#\\." "-back-slant33-que")
-                      
-                      (str/replace "#\\\"" #_"#\\\"" "-back-slant34-que")
+                      (str/replace "#\\\"" "-back-slant34-que")
                       (str/replace "#\\'" "-back-slant35-que")
                       (str/replace "#\\<" "-back-slant36-que")
-                      (str/replace "#\\>" "-back-slant37-que")
-                      ;;(str/replace "#\\\." "-back-slant33-que")
-                      ;;(str/replace "#\\/" "-back-slant33-que-")
-                      ))
+                      (str/replace "#\\>" "-back-slant37-que")))
                 list-init (fn [st] (str "( " st " )"))
                 code-list (->>
                            (slurp file-name)
@@ -54,27 +49,23 @@
     (for [file-name file-names]
       (split-code file-name))))
 
-#_(def scheme-ast (-> "lib/scheme-jimw-code/ydiff/htmlize.rkt" slurp
+(def scheme-ast-eg (first (read-string-for-pro (fn [code-list file-name] code-list) "ydiff")))
 
-                    (fn [st] (str "'(" st ")"))
-                    read-string))
-
-;;(read-string (str "( " (slurp "lib/scheme-jimw-code/ydiff/htmlize.rkt") " )"))
-
-(defn is-rkt-def
+(defn is-rkt-define
   [li]
-  li)
+  (= (first li) (symbol "define")))
 
-;; (import-scheme-def-and-other scheme-ast "file-name" #(print %) #(print %))
-(defn import-scheme-def-and-other
+;; TODOS: 把postwalk做成一个结构搜索: 搜索define结构, **复合算法结构
+;; (search-scheme-def-and-other scheme-ast-eg "file-name" #(print %) #(print (str "======" %)))
+(defn search-scheme-def-and-other
   [scheme-ast file-name save-def-fn save-other-fn]
   (let [res (clojure.walk/postwalk
              #(if (coll? %)
                 (do
-                  (if (is-rkt-def %)
-                    (do
-                      (save-def-fn (pp/write % :dispatch pp/code-dispatch :stream nil))
-                      (list :def-scheme-function (second %)))
-                    %))  %) scheme-ast)]
+                  (if (is-rkt-define %) #_(is-rkt-def %)
+                      (do
+                        (save-def-fn (pp/write % :dispatch pp/code-dispatch :stream nil))
+                        (list :def-scheme-function (second %)))
+                      %))  %) scheme-ast)]
     (save-other-fn
      (pp/write res :dispatch pp/code-dispatch :stream nil))))
