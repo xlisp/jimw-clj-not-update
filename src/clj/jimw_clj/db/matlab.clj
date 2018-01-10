7d9a6d75f2bfac72adde604dcb4589e486891f03
Steve Chan <chanshunli@gmail.com>
2018-01-10 23:54:53 +0800

    语言解析为AST的例子

--- src/clj/jimw_clj/db/matlab.clj
+++ src/clj/jimw_clj/db/matlab.clj
@@ -5,3 +5,36 @@
    [clojure.pprint :as pp]
    [taoensso.timbre :refer [error debug info]]
    [instaparse.core :as insta]))
+
+;; (bodol-parser "(map (λ a → (+ a 1)))")
+;; => ([:LIST [:SYMBOL "map"] [:LAMBDA [:CLAUSE [:ARGS [:SYMBOL "a"]] [:BODY [:LIST [:SYMBOL "+"] [:SYMBOL "a"] [:NUMBER "1"]]]]]])
+(def bodol-parser
+  (insta/parser
+   "
+<PROGRAM> = SPACE* SEXPS SPACE*
+<SEXPS> = (SEXP SPACE+)* SEXP
+<SEXP> = (QUOTED / DEFUN / LAMBDA / DOTTED / LIST / VECTOR / SYMBOL / NUMBER / STRING / BOOLEAN)
+QUOTED = <QUOTE> SEXP
+QUOTE = <'\\''>
+DOTTED = <'('> SPACE* SEXP SPACE <'.'> SPACE SEXP SPACE* <')'>
+LIST = <'('> SPACE* !((DEFUN_KEYWORD | LAMBDA_KEYWORD) (SPACE+ | <')'>)) SEXPS* SPACE* <')'>
+VECTOR = <'['> SPACE* SEXPS* SPACE* <']'>
+DEFUN = <'('> SPACE* <DEFUN_KEYWORD> SPACE+ SYMBOL SPACE+ CLAUSES SPACE* <')'>
+DEFUN_KEYWORD = 'ƒ' | 'defn' | 'defun'
+LAMBDA = <'('> SPACE* <LAMBDA_KEYWORD> SPACE+ CLAUSES SPACE* <')'>
+LAMBDA_KEYWORD = 'λ' | 'fn' | 'lambda'
+<CLAUSES> = (CLAUSE SPACE+)* CLAUSE
+CLAUSE = ARGS ARROW SPACE+ BODY
+ARGS = (!(ARROW SPACE+) SEXP SPACE+)*
+<ARROW> = <'->'> | <'→'>
+BODY = SEXP
+BOOLEAN = '#t' | '#f'
+NUMBER = NEGATIVE* (FRACTION | DECIMAL | INTEGER)
+<NEGATIVE> = '-'
+<FRACTION> = INTEGER '/' INTEGER
+<DECIMAL> = INTEGER '.' INTEGER
+<INTEGER> = #'\\p{Digit}+'
+STRING = '\\\"' #'([^\"\\\\]|\\\\.)*' '\\\"'
+SYMBOL = #'[\\pL_$&/=+~:<>|§?*-][\\pL\\p{Digit}_$&/=+~.:<>|§?*-]*'
+<SPACE> = <#'[ \t\n,]+'>
+"))
