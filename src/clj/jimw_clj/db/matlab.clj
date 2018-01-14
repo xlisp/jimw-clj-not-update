b1ec46476c50698354a3a7209fcaa1635ed6ad45
chanshunli <chanshunli@gmail.com>
2018-01-14 12:55:33 +0800

    无法分解文件的时候,只能独立开文件了

--- src/clj/jimw_clj/db/matlab.clj
+++ src/clj/jimw_clj/db/matlab.clj
@@ -5,3 +5,15 @@
    [clojure.pprint :as pp]
    [taoensso.timbre :refer [error debug info]]
    [instaparse.core :as insta]))
+
+;; db.core: (matlab/import-project-file-to-blog conn create-blog)
+(defn import-project-file-to-blog
+  [db create-blog]
+  (let [file-names
+        (->
+         (shell/sh "find" "lib" "-name" "*.m")
+         :out
+         (clojure.string/split #"\n"))
+        content-fn (fn [file-name] (str "```matlab\n" (slurp file-name) "\n```"))]
+    (for [file-name file-names]
+      (create-blog {:db db :name file-name :content (content-fn file-name)}))))
