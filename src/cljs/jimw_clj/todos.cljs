01d8350f428e46d39131c82502da000d7d7118c9
Steve Chan <chanshunli@gmail.com>
2018-01-24 11:44:03 +0800

    本地都没事,线上有问题: Uncaught SyntaxError: Unexpected end of input

--- src/cljs/jimw_clj/todos.cljs
+++ src/cljs/jimw_clj/todos.cljs
@@ -295,7 +295,7 @@
          [:section#todoapp
           [:header#header
            (new-todo blog-list blog-id items parid-first-id)]
-          #_[:div {:class "bdsug" :id "bdsug-search"}
+          [:div {:class "bdsug" :id "bdsug-search"}
            [:ul 
             [:li {:data-key "哒哒加速器", :class "bdsug-overflow"}
              "哒哒加速器"] 
