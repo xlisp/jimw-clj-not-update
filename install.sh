07c125e8a5232ec5fbf239ed0627bc5fe7b86fe5
chanshunli <chanshunli@gmail.com>
2018-03-28 02:25:50 -0400

    Add production install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Update code..."
+cd ~/jimw-clj && git pull origin master
+echo "Restart jimw-clj..."
+cd ~/jimw-clj && cp ~/app.js ./target/cljsbuild/public/js/app.js
+cd ~/jimw-clj &&  pm2 stop jimw-clj
+sleep 2
+cd ~/jimw-clj && pm2 start processes.json
+
