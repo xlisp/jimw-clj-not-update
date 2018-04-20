33996da96c56368b7174337538085c6612d8d394
Steve Chan <chanshunli@gmail.com>
2018-04-20 11:07:03 +0800

    采用ruby2.3.0解析ast

--- bin/ruby-parse-batch.sh
+++ bin/ruby-parse-batch.sh
@@ -1,8 +1,15 @@
 
 ## Usage: bash bin/ruby-parse-batch.sh lib/ruby-jimw-code/rails/
 rbpath=$1
-# sudo gem install parser
-# sudo gem install unparser
+
+# $ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# $ curl -sSL https://get.rvm.io | bash -s stable
+# $ source ~/.rvm/scripts/rvm
+# $ rvm install 2.3.0
+# $ rvm use 2.3.0 --default
+
+# $ gem install parser
+# $ gem install unparser
 
 echo "Clear AST file..."
 find $rbpath -name '*.ast' | xargs -I file rm file
