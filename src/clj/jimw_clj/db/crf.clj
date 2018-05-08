564dd1d9d68a4206c3f606f9b7a4a5cb7f131368
Steve Chan <chanshunli@gmail.com>
2018-05-08 20:39:06 +0800

    提取所有的函数名OK

--- src/clj/jimw_clj/db/crf.clj
+++ src/clj/jimw_clj/db/crf.clj
@@ -18,3 +18,65 @@
              (sql/call :array_cat :search_events
                        (honeysql.types/array [eid]))})
            (h/where [:= :id blog]))))")
+
+(comment
+
+  (clojure.walk/postwalk
+   #(cond
+      (list? %) (first %)    
+      :else %)
+   (read-string crf-code)
+   ) ;;=> defn
+
+  ;; TODO: 将树形的代码打成向量
+  (clojure.walk/postwalk
+   #(if (coll? %)
+      (prn %)
+      %)
+   (read-string crf-code)
+   )
+  ;; =========>>>
+  ;; [db blog eid]
+  ;; [:keys nil]
+  ;; {}
+  ;; [nil]
+  ;; (h/update :blogs)
+  ;; [eid]
+  ;; (honeysql.types/array nil)
+  ;; (sql/call :array_cat :search_events nil)
+  ;; [:search_events nil]
+  ;; {}
+  ;; (h/sset nil)
+  ;; [:= :id blog]
+  ;; (h/where nil)
+  ;; (-> nil nil nil)
+  ;; (jc1 db nil)
+  ;; (defn add-search-event-for-blog nil nil)
+  ;; 
+
+  ;; TODO: 提取出所有函数名,给这些函数加上函数的词性的标注
+  (clojure.walk/postwalk
+   #(if (coll? %)
+      ;;(prn (first %))
+      (if (list? %)
+        (prn (first %))      
+        ;;"not_function"
+        nil)
+      ;;
+      %)
+   (read-string crf-code)
+   )
+  ;; ===>>> OK: 提取所有的函数名
+  ;; h/update
+  ;; honeysql.types/array
+  ;; sql/call
+  ;; h/sset
+  ;; h/where
+  ;; ->
+  ;; jc1
+  ;; defn
+  ;; 
+
+
+
+  )
