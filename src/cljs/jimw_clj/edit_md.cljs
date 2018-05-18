4c9941817d8d81b9dca38de08d31c72121fdf979
Steve Chan <chanshunli@gmail.com>
2018-05-18 11:04:13 +0800

    原来双击编辑博客正文的方式 => 改为点击按钮编辑和保存博客正文: 因为编辑时搜索就保存了是使用不方便的,其次是手机上双击太容易编辑到博客了

--- src/cljs/jimw_clj/edit_md.cljs
+++ src/cljs/jimw_clj/edit_md.cljs
@@ -5,7 +5,7 @@
 
 (defn blog-content-input-par [{:keys [id name on-save on-stop]}]
   (let [val (r/atom name)
-        stop #(do (reset! val "")
+        stop #(do ;;(reset! val "")
                   (if on-stop (on-stop)))
         save #(let [v (-> @val str clojure.string/trim)]
                 (if-not (empty? v) (on-save v))
@@ -26,15 +26,29 @@
   (let [editing (r/atom false)]
     (fn [{:keys [id name save-fn]}]
       [:li.liststyle {:class (str (if @editing "editing"))}
+       [:button.btn.margin-download
+        {:on-click #(do
+                      (if @editing
+                        (do ;;(js/alert "保存博客")
+                          (reset! editing false))
+                        (do (reset! editing true)
+                            (js/setTimeout (fn []
+                                             (js/autosize (.querySelector js/document "textarea"))) 500)))
+                      )}
+        (if @editing
+          "Save Blog"
+          "Edit Blog")]
+       [:p]
        [:div.view
-        [:div {:on-double-click
-               #(do (reset! editing true)
-                    (js/setTimeout (fn []
-                                     (js/autosize (.querySelector js/document "textarea"))) 500))
+        [:div {;;:on-double-click
+               #_(do (reset! editing true)
+                     (js/setTimeout (fn []
+                                      (js/autosize (.querySelector js/document "textarea"))) 500))
                :dangerouslySetInnerHTML
                {:__html (js/marked name)}}]]
        (when @editing
          [blog-content-edit {:class "edit"
                              :on-save  #(save-fn id %)
                              :name name
-                             :on-stop #(reset! editing false)}])])))
+                             ;;:on-stop #(reset! editing false)
+                             }])])))
