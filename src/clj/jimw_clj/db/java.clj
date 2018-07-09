cc24d4be34bb349db8253c041ab299fdd27f8f68
Steve Chan <chanshunli@gmail.com>
2018-07-09 16:19:05 +0800

    add import-project-methods-to-blog

--- src/clj/jimw_clj/db/java.clj
+++ src/clj/jimw_clj/db/java.clj
@@ -21,3 +21,35 @@
       (create-blog {:db db :name file-name :content (content-fn file-name)
                     :source_type stype}))))
 
+(defn import-project-methods-to-blog
+  [db create-blog project stype ltype]
+  (let [project "clojure"
+        file-names
+        (->
+         (shell/sh "find"
+                   (str "lib/java-jimw-code/" project)
+                   "-name" "*.java.ast")
+         :out
+         (clojure.string/split #"\n"))
+        content-fn (fn [file-name] (str "```java\n" (slurp file-name) "\n```"))]
+    (doseq [file-name file-names]
+      (let [fcontent (slurp file-name)]
+        (if (empty? fcontent)
+          (prn (str file-name "----------空文件!"))
+          (let [javas (clojure.string/split (slurp file-name)
+                                            #"-----------split-line-----------------" )]
+            (doseq [method javas]
+              (do
+                (prn (str file-name "-----------------"))
+                (create-blog {:db db :name file-name
+                              :content (content-fn file-name)
+                              :source_type stype
+                              :language_type ltype})
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
